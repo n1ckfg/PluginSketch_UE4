61db8238087f55ded28233aea338fec380823220
n1ckfg <nick@fox-gieg.com>
2018-07-30 13:10:09 -0400

    first

---
+++ Source/PluginSketch_UE4/PluginSketch_UE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PluginSketch_UE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginSketch_UE4, "PluginSketch_UE4" );
---
+++ Source/PluginSketch_UE4/PluginSketch_UE4GameModeBase.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PluginSketch_UE4GameModeBase.h"
+
+
+
+
