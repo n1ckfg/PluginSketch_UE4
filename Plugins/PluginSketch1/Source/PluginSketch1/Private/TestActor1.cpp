d8e1d18539e951dcf09fa697f83c7bc8f9f1f622
n1ckfg <nick@fox-gieg.com>
2018-08-15 18:44:47 -0400

    Tests work

--- Plugins/PluginSketch1/Source/PluginSketch1/Private/TestActor1.cpp
+++ Plugins/PluginSketch1/Source/PluginSketch1/Private/TestActor1.cpp
@@ -3,25 +3,40 @@
 #include "TestActor1.h"
 
 // Sets default values
-ATestActor1::ATestActor1() {
+ATestActor1::ATestActor1() 
+{
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RootComponent"));
 	RootComponent = Mesh;
+
+	static ConstructorHelpers::FObjectFinder<UBlueprint> finder_BP_Blimp(TEXT("Blueprint'/Game/Blueprints/BP_Blimp.BP_Blimp'"));
+	BP_Blimp = findBlueprint(finder_BP_Blimp);
 }
 
 // Called when the game starts or when spawned
-void ATestActor1::BeginPlay() { 
+void ATestActor1::BeginPlay() 
+{ 
 	Super::BeginPlay(); 
 
-	SetMesh(meshFilePath);
+	loadMesh(meshFilePath);
+
+	AActor* blimp = GetWorld()->SpawnActor(BP_Blimp);
 }
 
 // Called every frame
-void ATestActor1::Tick(float DeltaTime) { 
+void ATestActor1::Tick(float DeltaTime) 
+{ 
 	Super::Tick(DeltaTime); 
 }
 
-void ATestActor1::SetMesh(FString MeshName) {
-	UStaticMesh *MeshAsset = Cast<UStaticMesh>(
-		StaticLoadObject(UStaticMesh::StaticClass(), NULL, *MeshName));
+void ATestActor1::loadMesh(FString MeshName) 
+{	
+	UStaticMesh *MeshAsset = Cast<UStaticMesh>(StaticLoadObject(UStaticMesh::StaticClass(), NULL, *MeshName));
 	Mesh->SetStaticMesh(MeshAsset);
+}
+
+UClass* ATestActor1::findBlueprint(ConstructorHelpers::FObjectFinder<UBlueprint> finder)
+{
+	FString url = finder.Object->GetPathName();
+	UE_LOG(LogTemp, Warning, TEXT("Found Blueprint at url: %s"), *url);
+	return (UClass*)finder.Object->GeneratedClass;
 }
--- Plugins/PluginSketch1/Source/PluginSketch1/Public/TestActor1.h
+++ Plugins/PluginSketch1/Source/PluginSketch1/Public/TestActor1.h
@@ -6,30 +6,37 @@
 #include "Components/StaticMeshComponent.h"
 #include "Engine/StaticMesh.h"
 #include "GameFramework/Actor.h"
+#include "ConstructorHelpers.h"
+
 // this must always be the last include
 #include "TestActor1.generated.h"
 
 UCLASS()
-class PLUGINSKETCH1_API ATestActor1 : public AActor {
+class PLUGINSKETCH1_API ATestActor1 : public AActor 
+{
   GENERATED_BODY()
 
 public:
 	// Sets default values for this actor's properties
 	ATestActor1();
+	
+	UClass* findBlueprint(ConstructorHelpers::FObjectFinder<UBlueprint> finder);
+	TSubclassOf<class AActor> BP_Blimp;
 
 	UPROPERTY(EditAnywhere)
 	FString meshFilePath;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void loadMesh(FString MeshName);
+
+	FString formatBlueprintUrl(FString url);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UStaticMeshComponent *Mesh;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	void SetMesh(FString MeshName);
 
 };
